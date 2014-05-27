1d54b176a27ea0c1abb8b2621fe2d75d506615d2
nischayn22 <nischayn22@gmail.com>
2014-05-27 21:50:20 +0530

    Moved IP address to tools script

--- coins.js
+++ coins.js
@@ -4,7 +4,6 @@
       $( 'span.Z3988' ).each( function ( i, member ) {
           var attributes_str = ($(member).attr('title'));
           var rft_attribs = attributes_str.replace("ctx_ver=Z39.88-2004&", "");
-          rft_attribs = '&req.ip=132.174.88.129&' + rft_attribs;
           var oclc_url = "http://tools.wmflabs.org/local-reference-api/index.php";
 //          var oclc_url = "http://localhost/index.php";
 
